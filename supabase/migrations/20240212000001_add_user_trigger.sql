2aee49840c1ba5dbd0dfac57cdc309268cf1aef0
Yusei- <acmnhc@gmail.com>
2026-02-12 13:50:57 -0500

    migration add user profile

---
+++ supabase/migrations/20240212000001_add_user_trigger.sql
@@ -0,0 +1,13 @@
+-- Trigger for creating a profile on signup
+create or replace function public.handle_new_user()
+returns trigger as $$
+begin
+  insert into public.profiles (id, username, avatar_url)
+  values (new.id, new.raw_user_meta_data->>'username', new.raw_user_meta_data->>'avatar_url');
+  return new;
+end;
+$$ language plpgsql security definer;
+
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
