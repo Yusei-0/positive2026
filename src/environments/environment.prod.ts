e8c009baf6c95721c3d683c2f206fb9b7790a160
Yusei- <acmnhc@gmail.com>
2026-02-14 09:00:29 -0500

    fix: version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   apiur: 'https://www.positive-api.online/',
   supabase_url: 'https://fdvibuecxumnzhahwrws.supabase.co',
   supabase_anon:
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkdmlidWVjeHVtbnpoYWh3cndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MTU2OTIsImV4cCI6MjA4NjQ5MTY5Mn0.DNdO9xgz-7dKATq344SNRmojNeMt_thoFtmuHw5MD_o'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkdmlidWVjeHVtbnpoYWh3cndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MTU2OTIsImV4cCI6MjA4NjQ5MTY5Mn0.DNdO9xgz-7dKATq344SNRmojNeMt_thoFtmuHw5MD_o',
+    version: '0.3.0'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   supabase_url: 'https://fdvibuecxumnzhahwrws.supabase.co',
   supabase_anon:
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkdmlidWVjeHVtbnpoYWh3cndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MTU2OTIsImV4cCI6MjA4NjQ5MTY5Mn0.DNdO9xgz-7dKATq344SNRmojNeMt_thoFtmuHw5MD_o',
-  version: '0.2.0'
+  version: '0.3.0'
 };
 
 /*
