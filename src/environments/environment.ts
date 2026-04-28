7e6bea586f9083bf1533dac0d7a82ccd4724e652
yusei <acmnhc@gmail.com>
2026-04-28 10:33:42 -0400

    updatye environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
   production: false,
   apiur: 'https://www.positive-api.online/',
-  supabase_url: 'https://fdvibuecxumnzhahwrws.supabase.co',
+  supabase_url: 'https://qevzmpoizgdmpvpimfue.supabase.co',
   supabase_anon:
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkdmlidWVjeHVtbnpoYWh3cndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MTU2OTIsImV4cCI6MjA4NjQ5MTY5Mn0.DNdO9xgz-7dKATq344SNRmojNeMt_thoFtmuHw5MD_o',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFldnptcG9pemdkbXB2cGltZnVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNzMwMzksImV4cCI6MjA5Mjk0OTAzOX0.VGciCcmjbl7XgQEK3s9_BvsNpdtjUM_7FEAhth3dQfY',
   version: '0.3.0'
 };
 
